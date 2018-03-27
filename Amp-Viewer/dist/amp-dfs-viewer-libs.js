bd73e4c98988fa390380941b0c6caf101f3351ec
Multiyork-hp <simon.donovan@dfs.co.uk>
2018-03-27 15:41:43 +0100

    https test

--- Amp-Viewer/dist/amp-dfs-viewer-libs.js
+++ Amp-Viewer/dist/amp-dfs-viewer-libs.js
@@ -810,8 +810,8 @@ amp.conf = {
     "cache_window": 21600000,
     "default_size": 190,
     "client_id":null,
-    "di_basepath":'http://i1.adis.ws/',
-    "content_basepath": "http://c1.adis.ws/",
+    "di_basepath":'https://images.dfs.co.uk/',
+    "content_basepath": "https://c1.adis.ws/",
     "err_img":null
 };
 
@@ -1636,7 +1636,7 @@ var webCacheSize = function (data) {
      */
     returnAs = function(options, from, to){
         for (var i = 0; i < options.length; i++) {
-            var key = typeof options[i] == "object" && keys(options[i]); 
+            var key = typeof options[i] == "object" && keys(options[i]);
             for(var o in options[i]){
                 if(options[i].hasOwnProperty(o)){
                     for (var c = 0; c < diOptions.length; c++) {
@@ -2049,7 +2049,7 @@ amp.stats.bind = function(o) {
 var aPush = function (obj,fn){
     aEvents[obj] ? aEvents[obj].push(fn) :  aEvents[obj] = [fn];
 };
- 
+
 /**
  * Triggers an event and its callbacks
  * @method event
@@ -3891,13 +3891,13 @@ amp.stats.event = function(dom,type,event,value){
             var i = 0;
             do{
 				var img = $('<img class="amp-zoom-img">');
-				img.css(reset); 
-				this.wrapper.append(img); 
+				img.css(reset);
+				this.wrapper.append(img);
                 this.imgs.push(img);
                 i++;
             }while(i < (this._cycle && this._cycle.len));
 
-       
+
             if(this.options.responsive){
                 this.element.css({ height:'auto', width:'100%', maxWidth:'100%' });
             }
@@ -4041,7 +4041,7 @@ amp.stats.event = function(dom,type,event,value){
                         distanceAllowed = 20;
 
                     this._lastEvent = '';
-                    var start = function(e){ 
+                    var start = function(e){
                         //e.preventDefault();
                         self.parent.on("touchend touchcancel", $.proxy(end,self));
                         this.startTouchEvent = e;
@@ -4430,7 +4430,7 @@ amp.stats.event = function(dom,type,event,value){
                 this._loadImage();
                 return;
             }
- 
+
             if(this._zoomLoaded || this._zoomLoading)
                 return;
 
@@ -4698,7 +4698,7 @@ amp.stats.event = function(dom,type,event,value){
                 var index = $.inArray(current, this.options.zoom);
                 transform = transform[index];
             }
-            url = transform ? this._setTransforms(url, transform) : url; 
+            url = transform ? this._setTransforms(url, transform) : url;
             url = this._setWidth(url, {h:this._originalImage.height * zoomBy, w:this._originalImage.width * zoomBy});
             return url;
         },
@@ -4745,7 +4745,7 @@ amp.stats.event = function(dom,type,event,value){
             var u = url.split('?');
             return u[0];
         },
-        _loadImage:function(){ 
+        _loadImage:function(){
             if(this._imageLoaded || this._imageLoading)
                 return;
 
@@ -4756,7 +4756,7 @@ amp.stats.event = function(dom,type,event,value){
             this.element[0].src = '';
             this.element[0].src = src;
         },
-        _getNaturalSize:function(src){ 
+        _getNaturalSize:function(src){
             return {width: this.element[0].naturalWidth, height: this.element[0].naturalHeight};
         },
         _setTransforms: function(url, transforms){
@@ -5869,7 +5869,7 @@ amp.stats.event = function(dom,type,event,value){
                     return _self._calcSize();
                 }
             }(self),1);
-            
+
             if(this.options.responsive){
                 $(window).on("resize", function(_self) {
                     return function() {
@@ -6081,11 +6081,11 @@ amp.stats.event = function(dom,type,event,value){
                 o = $(e.currentTarget).offset(),
                 mx = coords.x - o.left,
                 my = coords.y - o.top;
-            
+
             if(this._started) {
                 return;
             }
-            
+
             if(this.options.gesture.enabled) {
                 if(e.originalEvent && e.originalEvent.touches && e.originalEvent.touches[0] ) {
                     if (e.originalEvent.touches.length!=this.options.gesture.fingers)
@@ -6841,4 +6841,3 @@ videojs.plugin('resolutions', function(options) {
     });
 });
 }
-
