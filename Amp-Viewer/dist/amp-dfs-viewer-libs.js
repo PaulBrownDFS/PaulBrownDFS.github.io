c13020225db6631dc23fce657f4f98c416d14b05
Multiyork-hp <simon.donovan@dfs.co.uk>
2018-03-27 17:13:58 +0100

    https image tests

--- Amp-Viewer/dist/amp-dfs-viewer-libs.js
+++ Amp-Viewer/dist/amp-dfs-viewer-libs.js
@@ -1276,6 +1276,7 @@ var jsonp =  amp.jsonp = function(url, name, success, error, transform, timeout)
     }, timeout);
 
     var src = url + "?" + transform + buildQueryString({deep:true, timestamp: movingCacheWindow(), arg: "'"+name+"'", func:"amp.jsonReturn"});
+      console.log(src);
     var script = amp.get.createScript(src, function(e) {
         amp.jsonReturn(name,{ status:'error',code: 404, message: "Not Found", name: name });
     });
