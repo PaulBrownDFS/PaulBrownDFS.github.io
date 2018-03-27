3befd7e2987a57d351753735ca137eaf63d7f45a
Multiyork-hp <simon.donovan@dfs.co.uk>
2018-03-27 15:53:53 +0100

    https image tests

--- Amp-Viewer/dist/amp-dfs-viewers.js
+++ Amp-Viewer/dist/amp-dfs-viewers.js
@@ -2771,7 +2771,7 @@ var IntegrationViewer = require('./integration-viewer/index');
 
 amp.createViewer = function viewerFactory(config) {
     var defaultConfig = {
-        server: 'https://i1.adis.ws',
+        server: 'https://images.dfs.co.uk',
         errImgName: 'img404',
         account: 'dfs',
         mobile: false,
