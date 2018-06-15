30fbcfb5abdc7cd98ba8ac77ef61226250f17832
Multiyork-hp <simon.donovan@dfs.co.uk>
2018-06-15 17:46:24 +0100

    changed id for banner

--- slider/renderTypes.js
+++ slider/renderTypes.js
@@ -2,7 +2,7 @@ var renderTypes = {
     "blog": "3f6a5662-1998-4cf0-b369-f8ee6a7f0e42",
     "image": "23edf964-b9a4-4eba-b246-5ae726b719e9",
     "text": "c6fe6238-4752-47bc-bc5e-3413d0d08d30",
-    "banner": "1dc05e15-e2cb-4b7a-84d6-354bc31aae7b",
+    "banner": "a6f456a1-89b1-4fe5-8878-b536292be8ac",
     "link": "7b908316-30d9-409e-8ff7-c23fa8526cfe",
     "video": "eac8a783-932e-4900-bd96-bcaa84646499",
     "slider": "7940d31f-513c-405b-b7b8-f60e6af8420a",
@@ -17,3 +17,4 @@ var renderTypes = {
 
 
 // "image": "f0b1f9c8-9fbd-4470-aeb2-543c3cb01f19",
+// "banner": "1dc05e15-e2cb-4b7a-84d6-354bc31aae7b",
