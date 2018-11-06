a0359ad7391c0c4129c487b6310b614f82c359ce
Multiyork-hp <simon.donovan@dfs.co.uk>
2018-11-06 09:47:58 +0000

    fixed Espot 2 Typo of Variable Name

--- DFS_Slider/v2/carouselLoader_hpSlot.js
+++ DFS_Slider/v2/carouselLoader_hpSlot.js
@@ -387,7 +387,7 @@ if(!dfs){
 dfs.countdownv2 = {
   timerID: [],
   isMobile: function(){
-    if(window.location.origin.indexOf('https://m.dfs') > -1 ) {
+    if(window.location.origin.indexOf('https://m.dfs') > -1 || window.location.origin.indexOf('https://m.stage.live.dfs') > -1 ) {
       return true;
     }
     return false;
